7765307a795d35ea1f241873e27ecb689d03938a
Pranay Bhaturkar <47419296+pranay289@users.noreply.github.com>
2022-08-12 21:59:37 +0530

    The map<String,dynamic> error fixed

--- fwitch/lib/Feed Screen/views/feed_screen.dart
+++ fwitch/lib/Feed Screen/views/feed_screen.dart
@@ -18,65 +18,67 @@ class _FeedCreenState extends State<FeedCreen> {
   Widget build(BuildContext context) {
     return SafeArea(
         child: Padding(
-      padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
       child: Column(
         children: [
-          Text('Live Users '),
+          const Text('Live Users '),
           SizedBox(
             height: MediaQuery.of(context).size.height * 0.03,
           ),
-          StreamBuilder<dynamic>(
+          StreamBuilder<QuerySnapshot>(
             stream:
                 FirebaseFirestore.instance.collection('livestream').snapshots(),
             builder: (context, snapshot) {
               if (snapshot.connectionState == ConnectionState.waiting) {
-                return Center(
+                return const Center(
                   child: CircularProgressIndicator(),
                 );
+              } else {
+                return Expanded(
+                  child: ListView.builder(
+                    itemBuilder: (context, index) {
+                      final data = snapshot.data!.docs[index].data();
+                      LiveStream post =
+                          LiveStream.fromJson(data as Map<String, dynamic>);
+                      return InkWell(
+                        onTap: () {},
+                        child: Container(
+                          height: MediaQuery.of(context).size.height * 0.1,
+                          margin: const EdgeInsets.symmetric(vertical: 10),
+                          child: Row(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                AspectRatio(
+                                  aspectRatio: 16 / 9,
+                                  child: Image.network(post.image),
+                                ),
+                                const SizedBox(
+                                  width: 10,
+                                ),
+                                Column(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceEvenly,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(post.username),
+                                    Text(post.title),
+                                    Text('${post.viewers} watching'),
+                                    Text(
+                                      'Started ${timeago.format(post.startedAt.toDate())}',
+                                    )
+                                  ],
+                                ),
+                                IconButton(
+                                    onPressed: () {},
+                                    icon: Icon(Icons.more_vert))
+                              ]),
+                        ),
+                      );
+                    },
+                    itemCount: snapshot.data!.docs.length,
+                  ),
+                );
               }
-
-              return Expanded(
-                child: ListView.builder(
-                  itemBuilder: (context, index) {
-                    LiveStream post =
-                        LiveStream.fromMap(snapshot.data.docs[index].data);
-                    return InkWell(
-                      onTap: () {},
-                      child: Container(
-                        height: MediaQuery.of(context).size.height * 0.1,
-                        margin: EdgeInsets.symmetric(vertical: 10),
-                        child: Row(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              AspectRatio(
-                                aspectRatio: 16 / 9,
-                                child: Image.network(post.image),
-                              ),
-                              SizedBox(
-                                width: 10,
-                              ),
-                              Column(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceEvenly,
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(post.username),
-                                  Text(post.title),
-                                  Text('${post.viewers} watching'),
-                                  Text(
-                                    'Started ${timeago.format(post.startedAt.toDate())}',
-                                  )
-                                ],
-                              ),
-                              IconButton(
-                                  onPressed: () {}, icon: Icon(Icons.more_vert))
-                            ]),
-                      ),
-                    );
-                  },
-                  itemCount: snapshot.data.docs.length,
-                ),
-              );
             },
           )
         ],
